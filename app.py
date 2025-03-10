7e7a63cee2620aaf0c179186e173a80a1174883e
mohitbhoir789 <mohitbhoir789@gmail.com>
2025-03-10 17:17:50 -0400

    ui

--- app.py
+++ app.py
@@ -26,7 +26,8 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-image:'bg.png';
+        background-image: url("bg.png");
+        background-attachment: fixed;
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
